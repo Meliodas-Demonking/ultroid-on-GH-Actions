86162c39f5daa595f9c44eef5bbfbff8775413d8
Amirul Islam <amirulandalib9@gmail.com>
2022-05-02 21:28:34 +0600

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -4,4 +4,5 @@ cd /root/TeamUltroid
 pip install -U -r requirements.txt
 pip3 install --no-cache-dir -r requirements.txt
 pip3 install av --no-binary av
+pip3 install --no-cache-dir hiredis==2.0.0 && pip3 install --no-cache-dir redis==4.2.2
 bash startup
